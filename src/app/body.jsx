acfde401d457c8b143dd02b48fc060d12f94c5a3
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-10-18 17:53:21 -0300

    ice bro

--- src/app/body.jsx
+++ src/app/body.jsx
@@ -1,7 +1,50 @@
+import Image from "next/image"
+
 const Body = () => {
   return (
     <div className="w-full h-80 mt-40 border-b border-white border-opacity-10 mb-4">
-      <div className="bg-blue-800 p-36"></div>
+      <div className="bg-blue-900 p-24 flex justify-between">
+      <Image
+          src="/ice.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={30}
+          priority
+        />
+        <Image
+          src="/ice.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={30}
+          priority
+        />
+        <Image
+          src="/ice.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={30}
+          priority
+        />
+        <Image
+          src="/ice.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={30}
+          priority
+        />
+        <Image
+          src="/ice.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={30}
+          priority
+        />
+      </div>
     </div>
   )
 }
