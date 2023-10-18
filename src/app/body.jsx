1230c455b95be42c5386a6f143d026997a4bbdef
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-10-18 18:27:51 -0300

    fix body

--- src/app/body.jsx
+++ src/app/body.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Body = () => {
   return (
-    <div className="w-full h-80 mt-40 border-b border-white border-opacity-10 mb-4">
+    <div className="w-screen h-80 mt-40 border-b border-white border-opacity-10 mb-4">
       <div className="bg-blue-900 p-24 flex justify-between">
       <Image
           src="/ice.svg"
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Body from  './body'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center px-7 bg-slate-300 ">
+    <main className="flex min-h-screen flex-col items-center px-7 bg-slate-100">
       <Navbar/>
       <Body/>
       <Body/>
